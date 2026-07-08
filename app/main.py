ab352d730265c8bcf9780113c8095022d0e9e762
aqeelabbaskhan41 <aqeelabbaskhan41@gmail.com>
2026-07-08 18:14:57 +0500

    cors issue solved

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.chat import router as chat_router
 
 
 app = FastAPI()
+# add cors to allow all 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Chat API
 app.include_router(chat_router)
@@ -13,3 +22,5 @@ def home():
     return {
         "message": "Welcome to SoulPlus AI Backend!"
     }
+
+
